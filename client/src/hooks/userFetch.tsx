beb76ba357b5b86e71f3a3ab17b635bd164989fc
Sedulla-test-acc <n378580@gmail.com>
2022-07-05 08:12:57 +0400

    build: add useFetchData hook

---
+++ client/src/hooks/userFetch.tsx
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+// interface Props {}
+
+export const useFetchData = (url: any) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const res = await axios.get(url);
+        setData(res.data);
+      } catch (err: any) {
+        setError(err);
+      }
+      setLoading(false);
+    };
+  }, [url]);
+
+  const reFetchData = async () => {
+    setLoading(true);
+    try {
+      const res = await axios.get(url);
+      setData(res.data);
+    } catch (err: any) {
+      setError(err);
+    }
+    setLoading(false);
+  };
+
+  return { data, loading, error, reFetchData };
+};
